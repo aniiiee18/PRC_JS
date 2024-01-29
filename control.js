8c5e0f932012b88e13c59354284db259f09d5f20
aniketpanche <aniketpanche018@gmail.com>
2024-01-29 21:44:37 +0530

    control

---
+++ control.js
@@ -0,0 +1,47 @@
+// console.log("first commmit")
+// there are three statments
+
+//--------->IF STATEMENT<--------------
+
+// for(let i = 0; i<10; i++)
+// {
+//     if(i==5)
+//     {
+//         console.log("we have reched 5")
+//     }
+//     else
+//     {
+//         console.log(i);
+//             break
+
+//             // here the break statement will break the loop
+//     }
+
+    
+// }
+
+
+//--------------->SWITCH CASE<---------------------
+
+// for(let i = 0; i<=10; i++)
+// {
+//     switch(i)
+//     {   
+//         case 3:
+//             console.log(`${i} is a odd number`)
+//             break;
+
+//         case 5:
+//             console.log(`${i} is a odd number`)
+//             break;
+
+//         case 7:
+//             console.log(`${i} is a odd number`)
+//             break;
+
+//          case 9:
+//             console.log(`${i} is a odd number`)
+//             break;
+//     }
+//     console.log(i)
+// }
